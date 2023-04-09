1b2dea62787997f735f3b8e55f566072d4c82d60
RenJah <Khazix1313@gmail.com>
2023-04-09 22:43:57 +0300

    README.md added

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -2,24 +2,6 @@ from rest_framework import serializers
 from core.models import User, Post, Comment
 
 
-# User serializers
-# class UserCreateSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = [
-#             "id",
-#             "username",
-#             "password",
-#             "birth_date",
-#         ]
-#
-#     def create(self, validated_data):
-#         user = super().create(validated_data)
-#         user.set_password(user.password)
-#         user.save()
-#         return user
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
